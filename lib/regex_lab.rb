3f54352e707cffc414c99a1234476ad52d23197b
Blythe Pollard <blytheepollard@gmail.com>
2019-09-04 23:22:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ end
 end
 
 def valid_phone_number?(phone)
- if phone.scan(/[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})/)
+ if phone.scan(/(\d{10})[-. )]*(\d{3})[-. ]*(\d{4})/)
    true
  else false
 end
