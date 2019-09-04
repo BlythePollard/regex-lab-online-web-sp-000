81f85a435af20fa6f11574013cf30f8b8d6c720b
Blythe Pollard <blytheepollard@gmail.com>
2019-09-04 22:32:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,13 @@
 def starts_with_a_vowel?(word)
- word = "afoot Excellent incredible Absolute unreal Inconceivable octopus chandoo Crenel crooked Flimsy folksy Dog Hermit makeshift Palazzi sixfold Truth"
- if word.scan(/\w+^[aeiouAEIOU]/)
+ if word.match(/^[aeiouAEIOU]\w+/)
    true
-  elsif word.match(/\w+^[^aeiouAEIOU]/)
+  else 
   false
  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-#<<<<<<< HEAD
  "unassuming ambiguous understanding pomp circumstance uninteresting uncompromising grouchy corollary".scan(/\b[un]\w+\b/)
-
 end
 
 def words_five_letters_long(text)
@@ -18,9 +15,16 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/\b[A-Z]|[,.]/)
+    true
+  else
+    false
+end
 end
 
 def valid_phone_number?(phone)
-
+ if text.scan(/[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4}/)
+   true
+ else false
+end
 end
